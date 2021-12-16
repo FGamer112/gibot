a25ce19251b951c631050f12aaf9fe85014af447
your <your>
2021-12-16 13:17:14 -0500

    y

---
+++ refresh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/agentk/gitprojects/gibot
+git init
+git add .
+git commit -m 'y'
+git push https://FGamer112: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@github.com/FGamer112/gibot.git main
