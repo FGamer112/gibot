6397bc3a8ffef57df7f0d86430005050e4b2721a
FGamer112 <mailer2285@gmail.com>
2022-05-16 03:15:01 +0300

    y

---
+++ refresh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/agentk/gitprojects/gibot
+git init
+git add .
+git commit -m 'y'
+git push https://FGamer112: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@github.com/FGamer112/gibot.git main
